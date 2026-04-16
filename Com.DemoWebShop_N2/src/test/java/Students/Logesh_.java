0bc73ba63b23b202ea86c41603f8459deb5f8d2d
Logeshp1916 <logeshavm@gmail.com>
2026-04-16 20:21:43 +0530

    Commit by Logesh

--- Com.DemoWebShop_N2/src/test/java/Students/Logesh_.java
+++ Com.DemoWebShop_N2/src/test/java/Students/Logesh_.java
@@ -1,8 +1,15 @@
 package Students;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Logesh_ {
 	
-	
+	@Test
+	public void m1()
+	{
+		Reporter.log("Hi");
+	}
 	
 
 }
